f1f9dab1c93d2e160c54b5f38667658447f6e2b4
Sia <siakaramalegos@gmail.com>
2015-03-29 16:38:11 -0500

    Add validation for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  # Use allow_blank below to make sure we don't get multiple error messages for blankness since presence is validated above.
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for gif, jpg, or png image'
+  }
 end
